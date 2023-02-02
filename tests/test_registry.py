e4cfacd230847bd00beac065e6a38ad3462b93e9
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2023-02-02 13:39:06 -0500

    Fix tests

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -17,6 +17,9 @@ class TestUseCaseRegistry:
         state = registry.get_state()
         assert state == [10, 39]
 
+        registry.prune_state()
+        assert registry.is_empty()
+
     def test_adding_elements_of_different_type(self) -> None:
         """Test registry fails when adding objects of different types."""
         registry = UseCaseRegistry[str](max_length=2)
@@ -25,13 +28,6 @@ class TestUseCaseRegistry:
         with pytest.raises(ValueError):  # noqa: PT011
             registry.add_value(v=10)  # type:ignore[arg-type]
 
-    def test_registry_can_only_by_checked_once(self) -> None:
-        """Test registry fails when checked multiple times."""
-        registry = UseCaseRegistry[int](max_length=2)
-        registry.get_state()
-        with pytest.raises(RuntimeError):
-            registry.get_state()
-
     def test_registry_can_only_store_up_to_max_capacity(self) -> None:
         """Test registry fails when storage exceeds capacity."""
         registry = UseCaseRegistry[int](max_length=10)
@@ -39,4 +35,6 @@ class TestUseCaseRegistry:
             registry.add_value(v=value)
 
         with pytest.raises(RuntimeError):
-            registry.add_value(v=10)
+            registry.add_value(v=10)    
+    
+    
