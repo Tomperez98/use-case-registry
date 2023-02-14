7da897520bf71350bd6e06f6035a4a9c32e37399
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2023-02-14 14:20:11 -0500

    Deploy with documentation

--- tests/test_use_case.py
+++ tests/test_use_case.py
@@ -1,11 +1,11 @@
 """Test for use case."""
-from typing import Type
+from typing import Type, Union
 
 import pytest
 from result import Ok, Result
 
 from use_case_registry import UseCaseRegistry
-from use_case_registry.errors import IdentifiedError
+from use_case_registry.errors import IdentifiedError, NotIdentifiedError
 from use_case_registry.use_case import IUsecase
 
 
@@ -15,13 +15,12 @@ class TestIUsecase:
     def test_cannot_be_instantiated(self) -> None:
         """IUsecase is an interface an cannot be instantiated."""
         write_ops_registry = UseCaseRegistry[str](max_length=1)
-        result_registry = UseCaseRegistry[None](max_length=0)
+
         errors_registry = UseCaseRegistry[Type[IdentifiedError]](max_length=0)
 
         with pytest.raises(TypeError):
             IUsecase(
                 write_ops_registry=write_ops_registry,
-                result_registry=result_registry,
                 errors_registry=errors_registry,
             )  # type:ignore[abstract]
 
@@ -34,24 +33,24 @@ class TestIUsecase:
                 name: str,
                 last_name: str,
                 write_ops_registry: UseCaseRegistry[str],
-                result_registry: UseCaseRegistry[None],
                 errors_registry: UseCaseRegistry[Type[IdentifiedError]],
             ) -> None:
                 """Construct concrete implementation."""
                 self.name = name
                 self.last_name = last_name
-                super().__init__(write_ops_registry, result_registry, errors_registry)
+                super().__init__(write_ops_registry, errors_registry)
 
-            def execute(self) -> Result[None, Exception]:
+            def execute(
+                self,
+            ) -> Result[None, Union[IdentifiedError, NotIdentifiedError]]:
                 return Ok()
 
         write_ops_registry = UseCaseRegistry[str](max_length=1)
-        result_registry = UseCaseRegistry[None](max_length=0)
+
         errors_registry = UseCaseRegistry[Type[IdentifiedError]](max_length=0)
         ConcreteUsecase(
             name="Peter",
             last_name="Parket",
             write_ops_registry=write_ops_registry,
-            result_registry=result_registry,
             errors_registry=errors_registry,
         )
--- use_case_registry/use_case.py
+++ use_case_registry/use_case.py
@@ -1,10 +1,10 @@
 """Interface for concrete usecases."""
 import abc
-from typing import Any, Type
+from typing import Any, Type, Union
 
 from result import Result
 
-from .errors import IdentifiedError
+from .errors import IdentifiedError, NotIdentifiedError
 from .registry import UseCaseRegistry
 
 
@@ -14,14 +14,14 @@ class IUsecase(abc.ABC):
     def __init__(
         self,
         write_ops_registry: UseCaseRegistry[Any],
-        result_registry: UseCaseRegistry[Any],
         errors_registry: UseCaseRegistry[Type[IdentifiedError]],
     ) -> None:
         """use case constructor."""
         self.write_ops_registry = write_ops_registry
-        self.result_registry = result_registry
         self.errors_registry = errors_registry
 
     @abc.abstractmethod
-    def execute(self) -> Result[None, Exception]:
+    def execute(
+        self,
+    ) -> Result[Any, Union[IdentifiedError, NotIdentifiedError]]:
         """Workflow execution command to complete the use case."""
